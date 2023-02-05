efd7c69887b1f17a942a054507141eca8401aa77
Ashkley Zhao <zhaoashley2005@gmail.com>
2023-02-05 11:36:52 -0500

    stormHead fix and other assets

---
+++ TheJourneyOfOne/assets/levels/bigstatue.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="bigstatue" tilewidth="8" tileheight="8" tilecount="1620" columns="13">
+ <image source="bigstatue.png" width="108" height="240"/>
+</tileset>
---
+++ TheJourneyOfOne/assets/levels/log.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="log" tilewidth="8" tileheight="8" tilecount="192" columns="6">
+ <image source="log.png" width="50" height="37"/>
+</tileset>
